7edb5c0a702f382e297fc81fd7e4135cfffd0d41
kangping <wgtdkp@google.com>
2020-11-11 17:19:43 +0800

    tmp commit

---
+++ ServiceRegistration/srp-server.h
@@ -0,0 +1,10 @@
+#ifndef __SRP_SERVER_H
+#define __SRP_SERVER_H
+
+int srp_server_init(void *context);
+
+int srp_server_update(void *context);
+
+int srp_server_process(void *context);
+
+#endif // __SRP_SERVER_H
